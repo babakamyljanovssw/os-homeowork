6c703951a36086fa61c2e8d929778b863c002efb
babakamyljanovssw <babakamyljanov@ssw.com.au>
2024-11-14 10:09:44 +0800

    added comments

--- boot.asm
+++ boot.asm
@@ -1,4 +1,5 @@
-; program that prints alphabet in order
+; setting the ah register, which is BIOS interrupt 0x10
+; function for "Teletype output"
 mov ah, 0x0e
 mov al, 65
 int 0x10
@@ -12,5 +13,8 @@ loop:
 
 exit:
     jmp $
+
+; Fill the remaining bytes up to 510 with zeros, unsures teh code is exactly 512 bytes
+; adds boot signature, which tells BIOS that this code is bootable
 times 510-($-$$) db 0
 db 0x55, 0xaa
